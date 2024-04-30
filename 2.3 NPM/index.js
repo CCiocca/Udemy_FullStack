870c8e6659cce20ea3e68224e8481e1960939c72
CCiocca <chiara.cioccarelli1@gmail.com>
2024-05-01 01:12:42 +0200

    first commit

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+
+import {randomSuperhero} from 'superheroes';
+
+let hero = randomSuperhero();
+
+// import superheroes from 'superheroes'
+// let hero = superheroes.random();
+
+console.log(`I am ${hero}`);
