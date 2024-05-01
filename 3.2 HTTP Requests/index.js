9035b66c295db032fda769d7fc931c370e35245e
CCiocca <chiara.cioccarelli1@gmail.com>
2024-05-01 16:13:43 +0200

    express server and http requests

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -2,18 +2,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+app.get("/", (req, res) =>{
+res.send("hello from the server")
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+app.get("/about", (req,res) =>{
+  res.send("<h1>ABOUT page</h1>")
 });
 
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
-});
+app.get("/contacts", (req,res) => {
+  res.send("<h1>CONTACTS page</h1>")
+})
+
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`server running on port ${port}`)
 });
